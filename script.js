bce711c7be5cba438bded03504a0a232bb276f92
Shadow123466464 <163391746+Shadow123466464@users.noreply.github.com>
2026-02-06 21:48:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -259,15 +259,15 @@ document.addEventListener('DOMContentLoaded', function() {
         cursor.className = 'typewriter-cursor';
 
         const secretLetterContent = `
-        This Page is created by Baheeddine Dahen to the one girl he loved more than anything but couldn't be with Azza  
-        in this message i want to explain the idea of this creation it's letters in envelops contains a fonction that allows 
-        the number to be the same as the current month and everytime you refresh the letters changes also
-        do including the special background every letter can have one of the 31 special effects randomly also there is one letter selected
-        randomly everyday everytime you refresh or open the page it appears it's a daily selection.
+        This Page is created by Baheeddine Dahen to the one girl he loved more than anything but couldn't be with Azza.  
+        in this message i want to explain the idea of this creation it's letters and there is a fonction that allows 
+        the number of the envelopes to be the same as the current month, everytime you refresh the letters changes along with the
+        the special background every letter can have one of the 31 special effects randomly also there is one letter selected
+        randomly everyday, everytime you refresh or open the page it appears it's a daily selection.
         And for the reason i did that for because sometimes life can be hard and we can feel tired of everything and 
-        when you do feel like that you can open and read some to always remember that im here with 
-        you and ill do everything i could to make you smile or to prove how special you are for me 
-        and because i was never good with words i want you to see this and know how much i care i about you 
+        when you do feel like that you can open and read some to always remember that im here with you 
+        and ill do everything i could to make you smile or to prove how special you are for me 
+        and because i was never good with words i want you to see this and know how much i care about you 
         to create this for you and only you`;
 
         textContainer.classList.add('typewriter-container');
@@ -296,4 +296,5 @@ document.addEventListener('DOMContentLoaded', function() {
     createEnvelopes();
     createGalaxyAnimation();
 });
+
 
